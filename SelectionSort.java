d12d83d4ac18d5cd0ed0eec53077abcf16c322e2
Arya <85242388+aray7612354654654@users.noreply.github.com>
2021-11-03 13:44:21 -0400

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,1 +1,6 @@
-
+/*
+* In this file, implement Selection sort
+*
+* 1) In ascending order (left to right --> smallest to largest)
+* 2) In descending order (right to left --> largest to smallest)
+*/
