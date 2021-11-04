3b0595b7e0192e494c5c758f82773a8308fbf91a
Arya <85242388+aray7612354654654@users.noreply.github.com>
2021-11-04 17:14:00 -0400

    Finished

--- BubbleSort.java
+++ BubbleSort.java
@@ -11,6 +11,8 @@ public class BubbleSort {
     * The simplest sorting algorithm; works by repeatedly swapping the adjacent elements 
     * if they are in the wrong order.
     *
+    * O(n^2)
+    *
     * Example Array: {5, 1, 4, 2, 8}
     *
     * First Pass:
@@ -38,17 +40,21 @@ public class BubbleSort {
     /**
     * Sorts the inputted array from smallest to largest.
     */
-    public static void ascendingBubble(int[] intArr) {
-        int n = intArr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (intArr[j] > intArr[j + 1]) {
-                    int temp = intArr[j];
-                    intArr[j] = intArr[j + 1];
-                    intArr[j + 1] = temp;
+    public static void ascendingBubble(int[] array) {
+        for (int i = 0; i < array.length; i++) { // The outer loop makes sure that we iterate through the array enough times
+            for (int j = 1; j < array.length; j++) { // The inner loop takes care of comparing each value in the array
+                if (array[j] < array[j - 1]) { // Compares the current value to the previous one
+                    swap(array, j, j - 1);  // Swaps the two if the current value is less than the previous one
                 }
             }
         }
+    }
+    
+    // This is a private helper method that swaps two values in an array
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 
     /**
@@ -59,13 +65,39 @@ public class BubbleSort {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (intArr[j] < intArr[j + 1]) {
-                    int temp = intArr[j];
+                    /*int temp = intArr[j];
                     intArr[j] = intArr[j + 1];
-                    intArr[j + 1] = temp;
+                    intArr[j + 1] = temp;*/
+                    swap(intArr, j, j + 1);
                 }
             }
+        }       
+    }
+
+    // Wrote this one in notepad on my own for practice
+    public static void ascendingBubble2(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            } 
         }
-            
+    }
+
+    // Wrote this one in notepad on my own for practice
+    public static void descendingBubble2(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] > arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            } 
+        }
     }
 
     // Testing:
@@ -80,7 +112,7 @@ public class BubbleSort {
         System.out.println("]");
 
         // Performing an ascending Bubble Sort:
-        BubbleSort.ascendingBubble(testArr);
+        BubbleSort.ascendingBubble2(testArr);
         // Printing ascending Bubble Sort:
         System.out.println("Ascending Bubble Sort: [");
         for (int i = 0; i < testArr.length; i++) {
@@ -89,7 +121,7 @@ public class BubbleSort {
         System.out.println("]");
 
         // Performing a descending Bubble Sort:
-        BubbleSort.descendingBubble(testArr);
+        BubbleSort.descendingBubble2(testArr);
         // Printing descending Bubble Sort
         System.out.println("Descending Bubble Sort: [");
         for (int i = 0; i < testArr.length; i++) {
--- SelectionSort.java
+++ SelectionSort.java
@@ -4,3 +4,145 @@
 * 1) In ascending order (left to right --> smallest to largest)
 * 2) In descending order (right to left --> largest to smallest)
 */
+public class SelectionSort {
+
+    /**
+    * Sorts an array by repeatedly finding the currentimum element (for ascending)
+    * from unsorted part and putting it at the beginning. 
+    *
+    * The algorithm maintains two subarrays in a given array.
+    *   1) The subarray which is already sorted.
+    *   2) The subarray which is unsorted.
+    *
+    * In every iteration, the currentimum element (considering ascending) from the unsorted
+    * subarray is picked and moved to the sorted subarray.
+    *
+    * O(n^2)
+    */
+
+    /**
+     * Example:
+     * arr[] = {64, 25, 12, 22, 11}
+     *  
+     * Find the currentimum element in arr[0...4] and place it at beginning.
+     *  --> {[11], 25, 12, 22, 64}
+     * 
+     * Find current element in arr[1...4] and place it at beginning.
+     *  --> {[11, 12], 25, 22, 64}
+     * 
+     * Find the current element in arr[2...4] and place it at beginning.
+     *  --> {[11, 12, 22], 25, 64}
+     * 
+     * Find the current element in arr[3...4] and place it at beginning.
+     *  --> {[11, 12, 22, 25], 64}
+     * 
+     * arr[] = {[11, 12, 22, 25, 64]} <-- Our fully sorted list
+     */
+
+    public static void ascendingSelectionSort(int[] arr) {
+        int currentIndex = 0;
+        int currentVal = 0;
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentVal = arr[i];
+            currentIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < currentVal) {
+                    // swap(arr, i, j);
+                    currentVal = arr[j];
+                    currentIndex = j;
+                }
+            } 
+        
+            if (currentVal < arr[i]) {
+                temp = arr[i];
+                arr[i] = arr[currentIndex];
+                arr[currentIndex] = temp;
+            }
+        }
+    }
+
+    public static void descendingSelectionSort(int[] arr) {
+        int currentIndex = 0;
+        int currentVal = 0;
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentVal = arr[i];
+            currentIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] > currentVal) {
+                    // swap(arr, i, j);
+                    currentVal = arr[j];
+                    currentIndex = j;
+                }
+            } 
+        
+            if (currentVal > arr[i]) {
+                temp = arr[i];
+                arr[i] = arr[currentIndex];
+                arr[currentIndex] = temp;
+            }
+        }
+    }
+
+    public static void ascendingSelectionSortTry(int[] arr) {
+        int temp = 0;
+        int currentVal = 0;
+        int currentIdx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentVal = arr[i];
+            currentIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    currentVal = arr[j];
+                    currentIdx = j;
+                }
+            }
+        
+            if (currentVal < arr[i]) {
+                temp = arr[i];
+                arr[i] = arr[currentIdx];
+                arr[currentIdx] = temp;
+            }
+        }
+    }
+
+    /*private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }*/
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        System.out.print("[");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.print("]");
+
+        System.out.println("\n");
+
+        ascendingSelectionSort(arr);
+        System.out.println("Ascending selection sort:");
+        System.out.print("[");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.print("]");
+
+    
+        System.out.println("\n");
+
+
+        descendingSelectionSort(arr);
+        System.out.println("Descending selection sort");
+        System.out.print("[");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.print("]");
+    }
+}
